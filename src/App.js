ab244015b0191f5c2fcc4ed6c93e54e4ebb0e84c
shaikhsmsuss <shaikh.shoeb96@gmail.com>
2018-11-10 20:22:19 +0530

    here i included the Uservey component as it is a main file which will render the complete project.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Uservey from './Uservey';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Uservey/>
+      </div>
+    );
+  }
+}
+
+export default App;
