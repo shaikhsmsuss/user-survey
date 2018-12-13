e8a038facd6c95163effb3e2fd40b7a1c17290bf
shaikhsmsuss <shaikh.shoeb96@gmail.com>
2018-12-13 21:09:36 +0530

    update

--- src/Uservey.js
+++ src/Uservey.js
@@ -1,48 +1,48 @@
-import  React ,{Component} from 'react';
+import React, { Component } from 'react';
 
 var firebase = require('firebase');
-var uuid = require ('uuid');
+var uuid = require('uuid');
 var config = {
-    apiKey: "your firebase apiKey",
-    authDomain: "authDomain",
-    databaseURL: "database",
-    projectId: "projectId",
-    storageBucket: "",
-    messagingSenderId: "messagingSenderId"
+    apiKey: "AIzaSyDpqBgzTXeHORq1eAa0ICf2RHoVFL6W158",
+    authDomain: "uservey-26abb.firebaseapp.com",
+    databaseURL: "https://uservey-26abb.firebaseio.com",
+    projectId: "uservey-26abb",
+    storageBucket: "uservey-26abb.appspot.com",
+    messagingSenderId: "707863534463"
 };
 firebase.initializeApp(config);
 
 class Usurvey extends Component {
-    nameSubmit(event){
+    nameSubmit(event) {
         var studentName = this.refs.name.value;
-        this.setState({studentName: studentName}, function(){
+        this.setState({ studentName: studentName }, function() {
             console.log(this.state);
         });
     }
-    answerSelected(event){
+    answerSelected(event) {
         var answers = this.state.answers;
-        if(event.target.name === 'answer1'){
+        if (event.target.name === 'answer1') {
             answers.answer1 = event.target.value;
-        } else if(event.target.name === 'answer2'){
+        } else if (event.target.name === 'answer2') {
             answers.answer2 = event.target.value;
-        } else if(event.target.name === 'answer3'){
+        } else if (event.target.name === 'answer3') {
             answers.answer3 = event.target.value;
         }
 
-        this.setState({answers: answers}, function(){
+        this.setState({ answers: answers }, function() {
             console.log(this.state);
         });
     }
 
-    questionSubmit(){
-        firebase.database().ref('uSurvey/'+this.state.uid).set({
+    questionSubmit() {
+        firebase.database().ref('uSurvey/' + this.state.uid).set({
             studentName: this.state.studentName,
             answers: this.state.answers
         });
-        this.setState({isSubmitted: true});
+        this.setState({ isSubmitted: true });
     }
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -60,54 +60,106 @@ class Usurvey extends Component {
         this.questionSubmit = this.questionSubmit.bind(this);
     }
 
-    render(){
+    render() {
         var studentName;
         var questions;
 
-        if(this.state.studentName === '' && this.state.isSubmitted === false){
-            studentName = <div>
-                <h1>Hey Student, please let us know your name: </h1>
-                <form onSubmit={this.nameSubmit}>
-                    <input className="namy" type="text" placeholder="Enter your name" ref="name" />
-                </form>
-            </div>;
+        if (this.state.studentName === '' && this.state.isSubmitted === false) {
+            studentName = < div >
+                <
+                h1 > Hey Student, please
+            let us know your name: < /h1> <
+            form onSubmit = { this.nameSubmit } >
+                <
+                input className = "namy"
+            type = "text"
+            placeholder = "Enter your name"
+            ref = "name" / >
+                <
+                /form> < /
+                div > ;
             questions = ''
-        } else if (this.state.studentName !== '' && this.state.isSubmitted === false){
-            studentName = <h1>Welcome to U-Survey, {this.state.studentName}</h1>;
-            questions = <div>
-                <h2>Here are some questions: </h2>
-                <form onSubmit={this.questionSubmit}>
-                    <div className="card">
-                        <label>What kind of courses you like the most: </label> <br />
-                        <input type="radio" name="answer1" value="Technology" onChange={this.answerSelected} />Technology
-                        <input type="radio" name="answer1" value="Design" onChange={this.answerSelected} />Design
-                        <input type="radio" name="answer1" value="Marketing" onChange={this.answerSelected} />Marketing
-                    </div>
-                    <div className="card">
-                        <label>you are a: </label> <br />
-                        <input type="radio" name="answer2" value="Student" onChange={this.answerSelected} />Student
-                        <input type="radio" name="answer2" value="in-job" onChange={this.answerSelected} />in-job
-                        <input type="radio" name="answer2" value="looking-job" onChange={this.answerSelected} />looking-job
-                    </div>
-                    <div className="card">
-                        <label>Is Learning code is easy:  </label> <br />
-                        <input type="radio" name="answer3" value="yes" onChange={this.answerSelected} />yes
-                        <input type="radio" name="answer3" value="no" onChange={this.answerSelected} />no
-                        <input type="radio" name="answer3" value="maybe" onChange={this.answerSelected} />maybe
-                    </div>
-                    <input className="feedback-button" type="submit" value="submit" />
-                </form>
-            </div>
-        } else if(this.state.isSubmitted === true){
-            studentName = <h1>Thanks, {this.state.studentName}</h1>
+        } else if (this.state.studentName !== '' && this.state.isSubmitted === false) {
+            studentName = < h1 > Welcome to U - Survey, { this.state.studentName } < /h1>;
+            questions = < div >
+                <
+                h2 > Here are some questions: < /h2> <
+            form onSubmit = { this.questionSubmit } >
+                <
+                div className = "card" >
+                <
+                label > What kind of courses you like the most: < /label> <br / >
+                <
+                input type = "radio"
+            name = "answer1"
+            value = "Technology"
+            onChange = { this.answerSelected }
+            />Technology <
+            input type = "radio"
+            name = "answer1"
+            value = "Design"
+            onChange = { this.answerSelected }
+            />Design <
+            input type = "radio"
+            name = "answer1"
+            value = "Marketing"
+            onChange = { this.answerSelected }
+            />Marketing < /
+            div > <
+                div className = "card" >
+                <
+                label > you are a: < /label> <br / >
+                <
+                input type = "radio"
+            name = "answer2"
+            value = "Student"
+            onChange = { this.answerSelected }
+            />Student <
+            input type = "radio"
+            name = "answer2"
+            value = "in-job"
+            onChange = { this.answerSelected }
+            />in-job <
+            input type = "radio"
+            name = "answer2"
+            value = "looking-job"
+            onChange = { this.answerSelected }
+            />looking-job < /
+            div > <
+                div className = "card" >
+                <
+                label > Is Learning code is easy: < /label> <br / >
+                <
+                input type = "radio"
+            name = "answer3"
+            value = "yes"
+            onChange = { this.answerSelected }
+            />yes <
+            input type = "radio"
+            name = "answer3"
+            value = "no"
+            onChange = { this.answerSelected }
+            />no <
+            input type = "radio"
+            name = "answer3"
+            value = "maybe"
+            onChange = { this.answerSelected }
+            />maybe < /
+            div > <
+                input className = "feedback-button"
+            type = "submit"
+            value = "submit" / >
+                <
+                /form> < /
+                div >
+        } else if (this.state.isSubmitted === true) {
+            studentName = < h1 > Thanks, { this.state.studentName } < /h1>
         }
 
-        return(
-            <div>
-                {studentName}
-                --------------------------------
-                {questions}
-            </div>
+        return ( <
+            div > { studentName }
+            -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --{ questions } <
+            /div>
         );
     }
 }
